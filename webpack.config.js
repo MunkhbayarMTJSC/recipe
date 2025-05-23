0332dba2461c808b5adbbd7d2fcaf1ae7a691461
SiCA <munkhbayar.pch@gmail.com>
2025-05-23 20:24:05 +0800

    Anhnii huvilbar

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: './dist',
+      },
+    module: {
+        rules: [
+            {
+            test: /\.(?:js|mjs|cjs)$/,
+            exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    targets: "defaults",
+                        presets: [
+                            ['@babel/preset-env']
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+      
+    
+    plugins: [
+        new HtmlWebpackPlugin({  // Also generate a test.html
+          filename: 'index.html',
+          template: 'src/index.html'
+        })
+      ]
+    
+  
+};
